f512babe446d16b1ef832ba34d18935fba1cc371
LimTangmeng <limtangmeng100@gmail.com>
2022-11-13 11:35:39 +0700

    Fixed some bug

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import Home from "./Home";
 import Service from "./Service";
 import Project from "./Project";
 import Contact from "./Contact";
 
 export { Home, Service, Project, Contact };
-=======
-import Home from './Home';
-import Project from './Project';
-
-export { Home, Project };
->>>>>>> c91cb411667f819bdbcb2dde3d94dbcd7989ac7c
