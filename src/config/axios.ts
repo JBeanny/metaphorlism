119de3288432c3445e7a391d3ba4ad09c9d32fcd
LimTangmeng <limtangmeng100@gmail.com>
2022-12-02 14:42:24 +0700

    Add request to backend

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -16,14 +16,22 @@ const fetchData = async ({
   });
 };
 
-interface image {
+interface Image {
   name: string;
+  url: string;
+}
+
+interface ContactInfo {
+  platform: {
+    name: string;
+    image: string;
+  };
   url: string;
 }
 
 export interface IPackage {
   name: string;
-  image: image;
+  image: Image;
   description: string;
   serviceList: Array<string>;
   duration: number;
@@ -32,16 +40,44 @@ export interface IPackage {
 
 export interface IService {
   id: string;
-  image: image;
+  image: Image;
   name: string;
   description: string;
   package: Array<IPackage>;
+}
+
+export interface IContact {
+  name: string;
+  image: string;
+  contact: Array<ContactInfo>;
 }
 
 export async function getServices(): Promise<Array<IService>> {
   try {
     const result = await fetchData({ url: "/service" });
     return result.data;
+  } catch (err) {
+    throw new Error("Error");
+  }
+}
+
+interface getContactReponse {
+  team: IContact;
+  member: Array<IContact>;
+}
+
+export async function getContact(): Promise<getContactReponse> {
+  try {
+    const result = Promise.all([
+      fetchData({ url: "/metaphorlism" }),
+      fetchData({ url: "/member" }),
+    ]).then(([team, member]) => {
+      return {
+        team: team.data,
+        member: member.data,
+      };
+    });
+    return result;
   } catch (err) {
     throw new Error("Error");
   }
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,175 +1,101 @@
 import { useState } from "react";
 import { ContactContainer } from "../../components";
 import classes from "./index.module.css";
-import {
-  FacebookLogo,
-  LinkedInLogo,
-  MailLogo,
-  WebsiteLogo,
-  Sotharoth,
-  Tangmeng,
-  TelegramLogo,
-} from "../../assets";
+import { useQuery } from "react-query";
+import { getContact } from "../../config/axios";
 
 function Contact() {
   const [isTeam, setTeam] = useState(true);
-  const CONTACT_INFO = {
-    team: [
-      {
-        logo: FacebookLogo,
-        platform: "Facebook Page",
-        name: "Metaphorlism",
-        link: "https://facebook.com/Metaphorlism",
-        btnText: "Go to Facebook Page",
-      },
-      {
-        logo: LinkedInLogo,
-        platform: "LinkedIn",
-        name: "Metaphorlism",
-        link: "https://linkedin.com/in/metaphorlism",
-        btnText: "Go to LinkedIn",
-      },
-      {
-        logo: MailLogo,
-        platform: "Email",
-        name: "Metaphorlism",
-        link: "Metaphorlism@gmail.com",
-        btnText: "Send email to us",
-      },
-    ],
-    member: [
-      {
-        info: {
-          image: Sotharoth,
-          name: "Sotharoth Yim",
-        },
-        contact: [
-          {
-            logo: LinkedInLogo,
-            platform: "LinkedIn",
-            name: "Sotharoth Yim",
-            link: "https://linkedin.com/in/SotharothYim",
-            btnText: "Go to LinkedIn",
-          },
-          {
-            logo: WebsiteLogo,
-            platform: "Website",
-            name: "Sotharoth Yim",
-            link: "https://yim-sotharoth.vercel.app",
-            btnText: "Go to website",
-          },
-          {
-            logo: TelegramLogo,
-            platform: "Telegram",
-            name: "Sotharoth Yim",
-            link: "https://t.me/YimSotharoth",
-            btnText: "Contact me",
-          },
-        ],
-      },
-      {
-        info: {
-          image: Tangmeng,
-          name: "Tangmeng Lim",
-        },
-        contact: [
-          {
-            logo: LinkedInLogo,
-            platform: "LinkedIn",
-            name: "Tangmeng Lim",
-            link: "https://linkedin.com/in/TangmengLim",
-            btnText: "Go to LinkedIn",
-          },
-          {
-            logo: WebsiteLogo,
-            platform: "Website",
-            name: "Tangmeng Lim",
-            link: "https://limtangmeng.netlify.app",
-            btnText: "Go to website",
-          },
-          {
-            logo: TelegramLogo,
-            platform: "Telegram",
-            name: "Tangmeng Lim",
-            link: "https://t.me/TangmengLim",
-            btnText: "Contact me",
-          },
-        ],
-      },
-    ],
-  };
+  const { data, status } = useQuery(["contact"], getContact);
 
-  return (
-    <div className={classes.wrapper}>
-      <h1 className={classes.title}>
-        Get in touch with us for more information
-      </h1>
-      <p className={classes.sub}>
-        If you need help or have questions, we're here for you.
-      </p>
-      <div className={classes.contactContainer}>
-        <div
-          className={`${classes.team} ${isTeam ? classes.active : ""}`}
-          onClick={() => setTeam(true)}
-        >
-          Team Contact
+  if (status === "loading") return <h1>Loading</h1>;
+
+  if (status === "success")
+    return (
+      <div className={classes.wrapper}>
+        <h1 className={classes.title}>
+          Get in touch with us for more information
+        </h1>
+        <p className={classes.sub}>
+          If you need help or have questions, we're here for you.
+        </p>
+        <div className={classes.contactContainer}>
+          <div
+            className={`${classes.team} ${isTeam ? classes.active : ""}`}
+            onClick={() => setTeam(true)}
+          >
+            Team Contact
+          </div>
+          <div
+            className={`${classes.member} ${isTeam ? "" : classes.active}`}
+            onClick={() => setTeam(false)}
+          >
+            Member Contact
+          </div>
         </div>
-        <div
-          className={`${classes.member} ${isTeam ? "" : classes.active}`}
-          onClick={() => setTeam(false)}
-        >
-          Member Contact
-        </div>
+        {isTeam ? (
+          <div className={`${classes.contactWrapper} ${classes.team}`}>
+            {data.team.contact.map((team, i) => {
+              return (
+                <ContactContainer
+                  key={i}
+                  logo={`${import.meta.env.VITE_BACKEND_URL}${
+                    team.platform.image
+                  }`}
+                  platform={team.platform.name}
+                  name={data.team.name}
+                  link={team.url}
+                  btnText={
+                    team.platform.name.toLowerCase() !== "email"
+                      ? `Go to ${team.platform.name}`
+                      : "Send an Email"
+                  }
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <div className={classes.memberContainer}>
+            {data.member.map((member, i) => {
+              return (
+                <div key={i}>
+                  <div className={classes.head}>
+                    <img
+                      src={`${import.meta.env.VITE_BACKEND_URL}${member.image}`}
+                      alt={member.name}
+                      className={classes.image}
+                    />
+                    <h2 className={classes.name}>{member.name}</h2>
+                  </div>
+                  <div
+                    className={`${classes.contactWrapper} ${classes.member}`}
+                  >
+                    {member.contact.map((contact, i) => {
+                      return (
+                        <ContactContainer
+                          key={i}
+                          logo={`${import.meta.env.VITE_BACKEND_URL}${
+                            contact.platform.image
+                          }`}
+                          platform={contact.platform.name}
+                          name={member.name}
+                          link={contact.url}
+                          btnText={
+                            contact.platform.name.toLowerCase() !== "email"
+                              ? `Go to ${contact.platform.name}`
+                              : "Send an Email"
+                          }
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
-      {isTeam ? (
-        <div className={`${classes.contactWrapper} ${classes.team}`}>
-          {CONTACT_INFO.team.map((team, i) => {
-            return (
-              <ContactContainer
-                key={i}
-                logo={team.logo}
-                platform={team.platform}
-                name={team.name}
-                link={team.link}
-                btnText={team.btnText}
-              />
-            );
-          })}
-        </div>
-      ) : (
-        <div className={classes.memberContainer}>
-          {CONTACT_INFO.member.map((member, i) => {
-            return (
-              <div key={i}>
-                <div className={classes.head}>
-                  <img
-                    src={member.info.image}
-                    alt={member.info.name}
-                    className={classes.image}
-                  />
-                  <h2 className={classes.name}>{member.info.name}</h2>
-                </div>
-                <div className={`${classes.contactWrapper} ${classes.member}`}>
-                  {member.contact.map((contact, i) => {
-                    return (
-                      <ContactContainer
-                        key={i}
-                        logo={contact.logo}
-                        platform={contact.platform}
-                        name={contact.name}
-                        link={contact.link}
-                        btnText={contact.btnText}
-                      />
-                    );
-                  })}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default Contact;
